3b703d8ec90860d309a5777647c85edfc9d9247c
Yuri Skrypal <yjura@protonmail.com>
2024-12-04 17:07:50 +0200

    feat: add carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,20 +6,26 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') return false;
+
+  const converted = Number(sampleActivity);
+  if (!converted) return false;
+  if (converted > 15 || converted <= 0) return false;
+  const result = Math.ceil((Math.log(MODERN_ACTIVITY / converted) * HALF_LIFE_PERIOD) / Math.LN2);
+
+  return result;
 }
 
 module.exports = {
